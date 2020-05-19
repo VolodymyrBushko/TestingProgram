2a7aaca6c66e8a17af8084ec3fc49c0ae392a1e6
Volodymyr Bushko <Volodymyr.Bushko@gmail.com>
2020-05-19 10:39:44 +0300

    Add "Context" to Tables

---
+++ Tables/Context.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tables
+{
+    public class Context : DbContext
+    {
+        public Context(string connect)
+            :base(connect)
+        {
+        }
+
+        public virtual DbSet<Group> Groups { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<Test> Tests { get; set; }
+        public virtual DbSet<Result> Results { get; set; }
+    }
+}
