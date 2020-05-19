3fbe3b59c90818dd0f7f3bfc30f1b3650825e6cf
Volodymyr Bushko <Volodymyr.Bushko@gmail.com>
2020-05-19 21:38:44 +0300

    Add button "Remove" to AdminForm and other

--- Server/OtherForm/Admin/AdminForm.Designer.cs
+++ Server/OtherForm/Admin/AdminForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.panel1 = new System.Windows.Forms.Panel();
             this.buttonShowTests = new System.Windows.Forms.Button();
             this.buttonShowUsers = new System.Windows.Forms.Button();
@@ -37,13 +38,21 @@
             this.buttonAddUser = new System.Windows.Forms.Button();
             this.buttonAddTest = new System.Windows.Forms.Button();
             this.panel = new System.Windows.Forms.Panel();
+            this.labelNavigator = new System.Windows.Forms.Label();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
             this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.bindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.buttonRemove = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
+            this.panel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.buttonRemove);
             this.panel1.Controls.Add(this.buttonShowTests);
             this.panel1.Controls.Add(this.buttonShowUsers);
             this.panel1.Controls.Add(this.buttonShowGroups);
@@ -64,6 +73,7 @@
             this.buttonShowTests.TabIndex = 6;
             this.buttonShowTests.Text = "Show tests";
             this.buttonShowTests.UseVisualStyleBackColor = true;
+            this.buttonShowTests.Click += new System.EventHandler(this.buttonShow);
             // 
             // buttonShowUsers
             // 
@@ -73,6 +83,7 @@
             this.buttonShowUsers.TabIndex = 5;
             this.buttonShowUsers.Text = "Show users";
             this.buttonShowUsers.UseVisualStyleBackColor = true;
+            this.buttonShowUsers.Click += new System.EventHandler(this.buttonShow);
             // 
             // buttonShowGroups
             // 
@@ -82,6 +93,7 @@
             this.buttonShowGroups.TabIndex = 4;
             this.buttonShowGroups.Text = "Show groups";
             this.buttonShowGroups.UseVisualStyleBackColor = true;
+            this.buttonShowGroups.Click += new System.EventHandler(this.buttonShow);
             // 
             // buttonShowResults
             // 
@@ -91,6 +103,7 @@
             this.buttonShowResults.TabIndex = 3;
             this.buttonShowResults.Text = "Show results";
             this.buttonShowResults.UseVisualStyleBackColor = true;
+            this.buttonShowResults.Click += new System.EventHandler(this.buttonShow);
             // 
             // buttonAddGroup
             // 
@@ -125,10 +138,42 @@
             // panel
             // 
             this.panel.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel.Controls.Add(this.labelNavigator);
+            this.panel.Controls.Add(this.dataGridView);
             this.panel.Location = new System.Drawing.Point(162, 12);
             this.panel.Name = "panel";
             this.panel.Size = new System.Drawing.Size(310, 337);
             this.panel.TabIndex = 1;
+            // 
+            // labelNavigator
+            // 
+            this.labelNavigator.AutoSize = true;
+            this.labelNavigator.Location = new System.Drawing.Point(15, 9);
+            this.labelNavigator.Name = "labelNavigator";
+            this.labelNavigator.Size = new System.Drawing.Size(0, 13);
+            this.labelNavigator.TabIndex = 1;
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AllowUserToDeleteRows = false;
+            this.dataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(15, 29);
+            this.dataGridView.MultiSelect = false;
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(279, 279);
+            this.dataGridView.TabIndex = 0;
+            // 
+            // buttonRemove
+            // 
+            this.buttonRemove.Location = new System.Drawing.Point(3, 161);
+            this.buttonRemove.Name = "buttonRemove";
+            this.buttonRemove.Size = new System.Drawing.Size(101, 23);
+            this.buttonRemove.TabIndex = 7;
+            this.buttonRemove.Text = "Remove";
+            this.buttonRemove.UseVisualStyleBackColor = true;
+            this.buttonRemove.Click += new System.EventHandler(this.buttonRemove_Click);
             // 
             // AdminForm
             // 
@@ -139,7 +184,12 @@
             this.Controls.Add(this.panel1);
             this.Name = "AdminForm";
             this.Text = "AdminForm";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.AdminForm_FormClosing);
             this.panel1.ResumeLayout(false);
+            this.panel.ResumeLayout(false);
+            this.panel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -156,5 +206,9 @@
         private System.Windows.Forms.Button buttonAddTest;
         private System.Windows.Forms.Panel panel;
         private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.BindingSource bindingSource;
+        private System.Windows.Forms.Label labelNavigator;
+        private System.Windows.Forms.Button buttonRemove;
     }
 }
--- Server/OtherForm/Admin/AdminForm.cs
+++ Server/OtherForm/Admin/AdminForm.cs
@@ -50,13 +50,17 @@ namespace Server.OtherForm
                         tests = (Tests)serializer.Deserialize(stream);
                     }
 
-                    Tables.Test testTable = new Tables.Test();
-                    testTable.Title = tests.Subject;
-                    testTable.Path = $@"{pathToXml}\{file.Name}";
-                    testTable.Dt = DateTime.Parse(tests.Date);
-                    testTable.PassTime = int.Parse(tests.PassTime);
-                    testTable.User = user;
-                    work.Repository<Tables.Test>().Add(testTable);
+                    try
+                    {
+                        Tables.Test testTable = new Tables.Test();
+                        testTable.Title = tests.Subject;
+                        testTable.Path = $@"{pathToXml}\{file.Name}";
+                        testTable.Dt = DateTime.Parse(tests.Date);
+                        testTable.PassTime = int.Parse(tests.PassTime);
+                        testTable.User = user;
+                        work.Repository<Tables.Test>().Add(testTable);
+                    }
+                    catch { MessageBox.Show("Bad format of XML"); }
                 }
             }
         }
@@ -71,6 +75,85 @@ namespace Server.OtherForm
         {
             AddUserForm form = new AddUserForm(work);
             form.ShowDialog();
+        }
+
+        private async void buttonShow(object sender, EventArgs e)
+        {
+            await work.SaveAsync();
+
+            switch ((sender as Button).Text)
+            {
+                case "Show tests":
+                    bindingSource.DataSource = work.Repository<Tables.Test>().GetAll().Select(x => new
+                    {
+                        Id = x.Id,
+                        Title = x.Title,
+                        Date = x.Dt,
+                        PassTime = x.PassTime
+                    }).ToList();
+                    break;
+                case "Show users":
+                    bindingSource.DataSource = work.Repository<User>().GetAll().Select(x => new
+                    {
+                        Id = x.Id,
+                        Name = x.Name,
+                        Login = x.Login,
+                        Password = x.Password,
+                        Group = x.Group.Title
+                    }).ToList();
+                    break;
+                case "Show groups":
+                    bindingSource.DataSource = work.Repository<Group>().GetAll().Select(x => new
+                    {
+                        Id = x.Id,
+                        Title = x.Title
+                    }).ToList();
+                    break;
+                case "Show results":
+                    bindingSource.DataSource = work.Repository<Result>().GetAll().Select(x => new
+                    {
+                        Id = x.Id,
+                        Date = x.Dt,
+                        Rating = x.Rating,
+                        Test = x.Test.Title,
+                        Student = x.User.Login
+                    }).ToList();
+                    break;
+            }
+            dataGridView.DataSource = bindingSource;
+            labelNavigator.Text = (sender as Button).Text;
+        }
+
+        private void AdminForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            work.SaveChanges();
+        }
+
+        private void buttonRemove_Click(object sender, EventArgs e)
+        {
+            if (dataGridView.SelectedRows[0] != null)
+            {
+                int id = Convert.ToInt32(dataGridView.SelectedRows[0].Cells["Id"].Value);
+                switch (labelNavigator.Text)
+                {
+                    case "Show users":
+                        work.Repository<User>().Remove(work.Repository<User>().GetAll().FirstOrDefault(x => x.Id == id));
+                        break;
+                    case "Show groups":
+                        work.Repository<Group>().Remove(work.Repository<Group>().GetAll().FirstOrDefault(x => x.Id == id));
+                        break;
+                    case "Show results":
+                        work.Repository<Result>().Remove(work.Repository<Result>().GetAll().FirstOrDefault(x => x.Id == id));
+                        break;
+                    case "Show tests":
+                        Tables.Test test = work.Repository<Tables.Test>().GetAll().FirstOrDefault(x => x.Id == id);
+                        if (File.Exists(test.Path)) File.Delete(test.Path);
+                        work.Repository<Tables.Test>().Remove(test);
+                        break;
+                }
+                work.SaveChanges();
+                bindingSource.ResetBindings(false);
+            }
         }
     }
 }
