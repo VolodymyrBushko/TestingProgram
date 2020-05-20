828ff208c5ae6d354aa5d72c5380752204370c07
Volodymyr Bushko <Volodymyr.Bushko@gmail.com>
2020-05-20 11:52:40 +0300

    Add form "Client" and other

--- Server/OtherForm/Teacher/TeacherForm.Designer.cs
+++ Server/OtherForm/Teacher/TeacherForm.Designer.cs
@@ -48,7 +48,7 @@
             // listBoxTests
             // 
             this.listBoxTests.FormattingEnabled = true;
-            this.listBoxTests.Location = new System.Drawing.Point(92, 25);
+            this.listBoxTests.Location = new System.Drawing.Point(88, 25);
             this.listBoxTests.Name = "listBoxTests";
             this.listBoxTests.Size = new System.Drawing.Size(90, 199);
             this.listBoxTests.TabIndex = 0;
@@ -56,7 +56,7 @@
             // listBoxStudents
             // 
             this.listBoxStudents.FormattingEnabled = true;
-            this.listBoxStudents.Location = new System.Drawing.Point(188, 25);
+            this.listBoxStudents.Location = new System.Drawing.Point(184, 25);
             this.listBoxStudents.Name = "listBoxStudents";
             this.listBoxStudents.Size = new System.Drawing.Size(90, 199);
             this.listBoxStudents.TabIndex = 1;
@@ -65,7 +65,7 @@
             // 
             this.buttonSend.Location = new System.Drawing.Point(3, 25);
             this.buttonSend.Name = "buttonSend";
-            this.buttonSend.Size = new System.Drawing.Size(80, 23);
+            this.buttonSend.Size = new System.Drawing.Size(72, 23);
             this.buttonSend.TabIndex = 2;
             this.buttonSend.Text = "Send test";
             this.buttonSend.UseVisualStyleBackColor = true;
@@ -74,7 +74,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(91, 9);
+            this.label1.Location = new System.Drawing.Point(87, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(39, 13);
             this.label1.TabIndex = 3;
@@ -83,7 +83,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(187, 9);
+            this.label2.Location = new System.Drawing.Point(183, 9);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(55, 13);
             this.label2.TabIndex = 4;
@@ -97,22 +97,22 @@
             this.panel1.Controls.Add(this.buttonSend);
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(86, 262);
+            this.panel1.Size = new System.Drawing.Size(78, 262);
             this.panel1.TabIndex = 5;
             // 
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.panel2.Location = new System.Drawing.Point(84, 223);
+            this.panel2.Location = new System.Drawing.Point(77, 223);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(200, 39);
+            this.panel2.Size = new System.Drawing.Size(207, 39);
             this.panel2.TabIndex = 6;
             // 
             // buttonAddTest
             // 
             this.buttonAddTest.Location = new System.Drawing.Point(3, 64);
             this.buttonAddTest.Name = "buttonAddTest";
-            this.buttonAddTest.Size = new System.Drawing.Size(80, 23);
+            this.buttonAddTest.Size = new System.Drawing.Size(72, 23);
             this.buttonAddTest.TabIndex = 3;
             this.buttonAddTest.Text = "Add test";
             this.buttonAddTest.UseVisualStyleBackColor = true;
@@ -121,7 +121,7 @@
             // 
             this.buttonRemoveTest.Location = new System.Drawing.Point(3, 93);
             this.buttonRemoveTest.Name = "buttonRemoveTest";
-            this.buttonRemoveTest.Size = new System.Drawing.Size(80, 23);
+            this.buttonRemoveTest.Size = new System.Drawing.Size(72, 23);
             this.buttonRemoveTest.TabIndex = 4;
             this.buttonRemoveTest.Text = "Remove test";
             this.buttonRemoveTest.UseVisualStyleBackColor = true;
